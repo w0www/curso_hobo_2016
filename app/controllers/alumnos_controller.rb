8dbf4b11ac2049867678c7ac5ef54bd7ec283d21
w0www <imanol@alvarezperez.net>
2016-09-01 14:04:21 +0200

    Paso 6.2

--- app/controllers/alumnos_controller.rb
+++ app/controllers/alumnos_controller.rb
@@ -2,7 +2,7 @@ class AlumnosController < ApplicationController
 
   hobo_model_controller
 
-  auto_actions :all, :except => :index
+  auto_actions :all, :except => :show
 
   auto_actions_for :cursos, :create
 
